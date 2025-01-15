44fabef675c518d042b1c5dc07c62a6dd38eebdc
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-01-15 15:58:33 -0300

    feature: mui dark mode

--- app/home/page.js
+++ app/home/page.js
@@ -1,33 +1,51 @@
 // /pages/home.js
-'use client'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function HomePage() {
   const [spotifyData, setSpotifyData] = useState(null);
   const [error, setError] = useState(null);
-  const [type, setType] = useState('tracks');
-  const [period, setPeriod] = useState('short');
+  const [type, setType] = useState("tracks");
+  const [period, setPeriod] = useState("short");
   const [itemsData, setItemsData] = useState([]);
   const [accessToken, setAccessToken] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      const userName = Cookies.get('user_name');
+      const userName = Cookies.get("user_name");
       if (!userName) {
-        setError('User name cookie is missing');
+        setError("User name cookie is missing");
         return;
       }
 
-      setAccessToken(Cookies.get('spotify_access_token'))
+      setAccessToken(Cookies.get("spotify_access_token"));
 
       try {
-        const response = await axios.post('/api/userTracks', { user_name: userName });
+        const response = await axios.post("/api/userTracks", {
+          user_name: userName,
+        });
         setSpotifyData(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
-        setError('Failed to fetch data');
+        console.error("Error fetching data:", error);
+        setError("Failed to fetch data");
       }
     };
 
@@ -43,20 +61,23 @@ export default function HomePage() {
 
       const requests = data.map((item) => {
         const id = item[0];
-        const url = type === 'tracks' ? `https://api.spotify.com/v1/tracks/${id}` : `https://api.spotify.com/v1/artists/${id}`;
+        const url =
+          type === "tracks"
+            ? `https://api.spotify.com/v1/tracks/${id}`
+            : `https://api.spotify.com/v1/artists/${id}`;
         return axios.get(url, {
           headers: {
-            'Authorization': `Bearer ${accessToken}` // Reemplaza con tu token de acceso
-          }
+            Authorization: `Bearer ${accessToken}`, // Reemplaza con tu token de acceso
+          },
         });
       });
 
       try {
         const responses = await Promise.all(requests);
-        setItemsData(responses.map(response => response.data));
-        console.log(itemsData)
+        setItemsData(responses.map((response) => response.data));
+        console.log(itemsData);
       } catch (error) {
-        console.error('Error fetching item data:', error);
+        console.error("Error fetching item data:", error);
       }
     };
 
@@ -71,49 +92,65 @@ export default function HomePage() {
     setPeriod(e.target.value);
   };
 
-  const renderData = () => {
+  const renderList = () => {
     if (!itemsData.length) return <p>Loading data...</p>;
-    
-    return (<>
-      <ul>
-        {itemsData.map((item, index) => (
-          <li key={index}>
-            <pre>{JSON.stringify(item, null, 2)}</pre>
-          </li>
-        ))}
-      </ul>
+    return (
+      <>
+        <List>
+          {itemsData.map((item, index) => {
+            console.log(item)
+            return <ListItem key={item.index}
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                  {index+1}
+                </IconButton>
+              }
+            >
+              <ListItemAvatar>
+                <Avatar>12</Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Single-line item"
+                secondary="Secondary text"
+              />
+            </ListItem>;
+          })}
+        </List>
       </>
     );
   };
 
   return (
-    <div>
-      <h1>Home</h1>
-      <p>You have successfully logged in with Spotify!</p>
-      {error && <p>Error: {error}</p>}
-      {spotifyData ? (
-        <div>
-          <h2>Spotify Data</h2>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div>
+        <h1>Home</h1>
+        <p>You have successfully logged in with Spotify!</p>
+        {error && <p>Error: {error}</p>}
+        {spotifyData ? (
           <div>
-            <label htmlFor="type">Type: </label>
-            <select id="type" value={type} onChange={handleTypeChange}>
-              <option value="tracks">Tracks</option>
-              <option value="artists">Artists</option>
-            </select>
+            <h2>Spotify Data</h2>
+            <div>
+              <label htmlFor="type">Type: </label>
+              <select id="type" value={type} onChange={handleTypeChange}>
+                <option value="tracks">Tracks</option>
+                <option value="artists">Artists</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="period">Period: </label>
+              <select id="period" value={period} onChange={handlePeriodChange}>
+                <option value="short">Short</option>
+                <option value="medium">Medium</option>
+                <option value="long">Long</option>
+              </select>
+            </div>
+            {renderList()}
           </div>
-          <div>
-            <label htmlFor="period">Period: </label>
-            <select id="period" value={period} onChange={handlePeriodChange}>
-              <option value="short">Short</option>
-              <option value="medium">Medium</option>
-              <option value="long">Long</option>
-            </select>
-          </div>
-          {renderData()}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </ThemeProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,18 +1,33 @@
 // /pages/index.js
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function Home() {
-    const redirectToSpotify = () => {
-        const scope = 'user-top-read';
-        const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI)}`;
-        window.location.href = url;
-    };
+  const redirectToSpotify = () => {
+    const scope = "user-top-read";
+    const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${
+      process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID
+    }&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(
+      process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI
+    )}`;
+    window.location.href = url;
+  };
 
-    return (
-        <div>
-            <h1>Spotify Login</h1>
-            <button onClick={redirectToSpotify}>Authorize with Spotify</button>
-        </div>
-    );
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div>
+        <h1>Spotify Login</h1>
+        <button onClick={redirectToSpotify}>Authorize with Spotify</button>
+      </div>
+    </ThemeProvider>
+  );
 }
