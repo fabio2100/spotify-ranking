3659348ecf505115fecb38aba4dc63803a5aa00d
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-02-18 17:50:00 -0300

    fix: v1.1 ssl error database

--- db.js
+++ db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     database: process.env.PG_DATABASE, // Nombre de la base de datos
     password: process.env.PG_PASSWORD, // Contraseña de PostgreSQL
     port: process.env.PG_PORT || 5432, // Puerto de PostgreSQL (por defecto 5432)
+    ssl: {
+        rejectUnauthorized: false      // Si utilizas un certificado auto-firmado, establece esta opción a false
+    }
 });
 
 export default pool;
